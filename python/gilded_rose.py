198f1fe1a4db0488013c7115ee95697548becd4c
Ashutosh Kumar <ashutoshind2017@outlook.com>
2025-06-24 09:53:34 +0530

    Refactor updaters for clarity and maintainability, enforce quality bounds

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+MAX_QUALITY = 50
+MIN_QUALITY = 0
+SULFURAS_QUALITY = 80
+
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -9,7 +13,17 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+def clamp_quality(item, max_quality=MAX_QUALITY):
+    if item.name == "Sulfuras, Hand of Ragnaros":
+        item.quality = SULFURAS_QUALITY
+    else:
+        if item.quality < MIN_QUALITY:
+            item.quality = MIN_QUALITY
+        if item.quality > max_quality:
+            item.quality = max_quality
+
 class ItemUpdater:
+    """Base updater for normal items."""
     def __init__(self, item):
         self.item = item
 
@@ -18,57 +32,53 @@ class ItemUpdater:
         self.update_sell_in()
         if self.item.sell_in < 0:
             self.update_expired()
+        clamp_quality(self.item)
 
     def update_quality(self):
-        if self.item.quality > 0:
-            self.item.quality -= 1
+        self.item.quality -= 1
 
     def update_sell_in(self):
         self.item.sell_in -= 1
 
     def update_expired(self):
-        if self.item.quality > 0:
-            self.item.quality -= 1
+        self.item.quality -= 1
 
 class AgedBrieUpdater(ItemUpdater):
+    """Aged Brie increases in quality as it ages."""
     def update_quality(self):
-        if self.item.quality < 50:
-            self.item.quality += 1
+        self.item.quality += 1
 
     def update_expired(self):
-        if self.item.quality < 50:
-            self.item.quality += 1
+        self.item.quality += 1
 
 class SulfurasUpdater(ItemUpdater):
+    """Sulfuras never changes."""
     def update(self):
-        pass  # Legendary, does not change
+        self.item.quality = SULFURAS_QUALITY  # Always 80
+        pass
 
 class BackstagePassUpdater(ItemUpdater):
+    """Backstage passes increase in quality as sell_in approaches, drop to 0 after."""
     def update_quality(self):
-        if self.item.quality < 50:
+        if self.item.sell_in > 10:
             self.item.quality += 1
-            if self.item.sell_in < 11 and self.item.quality < 50:
-                self.item.quality += 1
-            if self.item.sell_in < 6 and self.item.quality < 50:
-                self.item.quality += 1
+        elif self.item.sell_in > 5:
+            self.item.quality += 2
+        elif self.item.sell_in > 0:
+            self.item.quality += 3
+        else:
+            self.item.quality = 0  # Will be clamped in update()
 
     def update_expired(self):
         self.item.quality = 0
 
 class ConjuredUpdater(ItemUpdater):
+    """Conjured items degrade in quality twice as fast as normal items."""
     def update_quality(self):
-        degrade = 2
-        if self.item.quality > 0:
-            self.item.quality -= degrade
-            if self.item.quality < 0:
-                self.item.quality = 0
+        self.item.quality -= 2
 
     def update_expired(self):
-        degrade = 2
-        if self.item.quality > 0:
-            self.item.quality -= degrade
-            if self.item.quality < 0:
-                self.item.quality = 0
+        self.item.quality -= 2
 
 def get_updater(item):
     if item.name == "Aged Brie":
